7750663575381f2fec214ccc45fb7a199dbff50d
Shariful Islam <engr.shariful.cse@gmail.com>
2022-10-27 06:43:38 +0200

    Cart payment method page has build

--- src/pages/CheckOut/CheckOut.js
+++ src/pages/CheckOut/CheckOut.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Button, Card, Col, Image, Row } from "react-bootstrap";
 import toast from "react-hot-toast";
 import { Link, useLoaderData } from "react-router-dom";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBInput,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 const CheckOut = () => {
   const checkOut = useLoaderData();
@@ -13,7 +22,7 @@ const CheckOut = () => {
   const totalBill = parseFloat(newPrice + vat);
 
   const handleBuy = () => {
-    toast.success("successfully completed");
+    toast.success("successfully Purchased");
   };
 
   return (
@@ -68,20 +77,153 @@ const CheckOut = () => {
                 </div>
               </div>
             </Card>
-            <div>
+            <div className="mb-5">
               <Link to="/courses">
                 <Button onClick={handleBuy} className="w-100 mt-5">
                   Buy Now {`${totalBill}`}
                 </Button>
               </Link>
-              {/* <Modal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-              ></Modal> */}
+            </div>
+            <div className="mt-5">
+              <h2>Payment Method</h2>{" "}
+              <MDBContainer
+                className="py-1"
+                fluid
+                style={{
+                  backgroundImage:
+                    "url(https://mdbcdn.b-cdn.net/img/Photos/Others/background3.webp)",
+                }}
+              >
+                <MDBRow className=" d-flex justify-content-center">
+                  <MDBCol>
+                    <MDBCard className="rounded-3">
+                      <MDBCardBody className="p-4">
+                        <div className="text-center mb-4">
+                          <h3>Settings</h3>
+                          <h6>Payment</h6>
+                        </div>
+                        <p className="fw-bold mb-4 pb-2">Saved cards:</p>
+                        <div className="d-flex flex-row align-items-center mb-4 pb-1">
+                          <img
+                            className="img-fluid"
+                            src="https://img.icons8.com/color/48/000000/mastercard-logo.png"
+                          />
+                          <div className="flex-fill mx-3">
+                            <div className="form-outline">
+                              <MDBInput
+                                label="Card Number"
+                                id="form1"
+                                type="text"
+                                size="lg"
+                                value="**** **** **** 3193"
+                              />
+                            </div>
+                          </div>
+                          <a href="#!">Remove card</a>
+                        </div>
+                        <div className="d-flex flex-row align-items-center mb-4 pb-1">
+                          <img
+                            className="img-fluid"
+                            src="https://img.icons8.com/color/48/000000/visa.png"
+                          />
+                          <div className="flex-fill mx-3">
+                            <div className="form-outline">
+                              <MDBInput
+                                label="Card Number"
+                                id="form2"
+                                type="text"
+                                size="lg"
+                                value="**** **** **** 4296"
+                              />
+                            </div>
+                          </div>
+                          <a href="#!">Remove card</a>
+                        </div>
+                        <p className="fw-bold mb-4">Add new card:</p>
+                        <MDBInput
+                          label="Cardholder's Name"
+                          id="form3"
+                          type="text"
+                          size="lg"
+                          value="Shariful Islam"
+                        />
+                        <MDBRow className="my-4">
+                          <MDBCol size="7">
+                            <MDBInput
+                              label="Card Number"
+                              id="form4"
+                              type="text"
+                              size="lg"
+                              value="1234 5678 1234 5678"
+                            />
+                          </MDBCol>
+                          <MDBCol size="3">
+                            <MDBInput
+                              label="Expire"
+                              id="form5"
+                              type="password"
+                              size="lg"
+                              placeholder="MM/YYYY"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2">
+                            <MDBInput
+                              label="CVV"
+                              id="form6"
+                              type="password"
+                              size="lg"
+                              placeholder="CVV"
+                            />
+                          </MDBCol>
+                        </MDBRow>
+                        <Link to="/courses">
+                          <Button onClick={handleBuy} className="w-100 mt-5">
+                            <h5>Purchase ${`${totalBill}`}</h5>
+                          </Button>
+                        </Link>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                </MDBRow>
+              </MDBContainer>
             </div>
           </div>
         </Col>
-        <Col lg="6"> Payment Information</Col>
+        <Col lg="6">
+          <div className="mt-5 ms-5 ps-5">
+            <h3>See what others have to say...</h3>
+            <div className="d-flex mt-5">
+              <img src="" alt="" />
+
+              <p className="ms-2 text-secondary  ">
+                This courses teach exactly what the titles promise to teach.
+                Quality of production, content, and lectures are excellent!
+                Explanations of the how and why are also a nice touch! -
+                Shariful Islam
+              </p>
+            </div>
+            <div className="d-flex mt-5">
+              <img src="" alt="" />
+
+              <p className="ms-2 text-secondary ">
+                this courses teach exactly what the titles promise to teach.
+                Quality of production, content, and lectures are excellent!
+                Explanations of the how and why are also a nice touch! -
+                Shariful Islam
+              </p>
+            </div>
+            <div className="d-flex mt-5">
+              <img src="" alt="" />
+
+              <p className="ms-2 text-secondary ">
+                this courses teach exactly what the titles promise to teach.
+                Quality of production, content, and lectures are excellent!
+                Explanations of the how and why are also a nice touch! -
+                Shariful Islam
+              </p>
+            </div>
+          </div>
+        </Col>
       </Row>
     </div>
   );
